00f1f7fab9ce5c091f9e6afaa115ba90886ddaa4
konradmh3 <konradm.h.3@gmail.com>
2024-09-27 14:24:15 -0700

    Fixed msg formatting

--- love-letter-app/src/app/create/letter/page.tsx
+++ love-letter-app/src/app/create/letter/page.tsx
@@ -1,8 +1,5 @@
-// 'use client';
-
 import { getMessage } from "@/app/lib/data";
 import AnimatedCard from "@/app/ui/animatedCard";
-// import { Suspense } from "react";
 
 export default async function Letter({
   searchParams,
@@ -15,8 +12,6 @@ export default async function Letter({
   const message = await getMessage(searchParams?.id);
   return (
     <div className="letter-container">
-      {/* <div  className="message">
-      {message.message}</div> */}
       <div className="animated-card">
         <AnimatedCard svg={searchParams?.svg} msg={message.message} />
       </div>
--- love-letter-app/src/app/lib/actions.ts
+++ love-letter-app/src/app/lib/actions.ts
@@ -4,8 +4,6 @@ import { sql } from "@vercel/postgres";
 
 
 export async function postMessage(message: string) {
-
-
     const result = await sql`INSERT INTO messages (message) VALUES (${message}) RETURNING id`;
     return result.rows[0].id;
 }   
