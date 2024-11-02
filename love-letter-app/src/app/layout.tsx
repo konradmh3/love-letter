a0828e25871ccb4b3c97e7ff7048c6258f2a87ec
konradmh3 <konradm.h.3@gmail.com>
2024-11-02 14:24:37 -0700

    Added some proper meta tags

--- love-letter-app/src/app/layout.tsx
+++ love-letter-app/src/app/layout.tsx
@@ -6,7 +6,12 @@ import { robotoSlab } from "./ui/fonts";
 export const metadata: Metadata = {
   title: "Love Letter",
   description: "Send a letter to loved one",
+  viewport: "width=device-width, initial-scale=1.0",
 };
+
+{/* <meta name="viewport" content="width=device-width, initial-scale=1.0"> */}
+
+
 
 export default function RootLayout({
   children,
