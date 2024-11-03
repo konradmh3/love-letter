3b77249d355d0de63c110316ecee9c03ad92b88a
konradmh3 <konradm.h.3@gmail.com>
2024-11-02 17:44:23 -0700

    Fixed viewport and adjusted hieght css for webview kotlin app

--- love-letter-app/src/app/layout.tsx
+++ love-letter-app/src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { robotoSlab } from "./ui/fonts";
+import { Viewport } from "next";
 
 
 export const metadata: Metadata = {
   title: "Love Letter",
   description: "Send a letter to loved one",
-  viewport: "width=device-width, initial-scale=1.0",
 };
 
-{/* <meta name="viewport" content="width=device-width, initial-scale=1.0"> */}
-
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+};
 
 
 export default function RootLayout({
