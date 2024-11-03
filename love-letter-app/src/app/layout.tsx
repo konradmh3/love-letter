52782a098dbea6c448856a0530e5afeb34d92dd3
konradmh3 <konradm.h.3@gmail.com>
2024-11-02 18:05:46 -0700

    Meta changes

--- love-letter-app/src/app/layout.tsx
+++ love-letter-app/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { robotoSlab } from "./ui/fonts";
 import { Viewport } from "next";
+import Head from "next/head";
+
 
 
 export const metadata: Metadata = {
@@ -9,10 +11,6 @@ export const metadata: Metadata = {
   description: "Send a letter to loved one",
 };
 
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1.0,
-};
 
 
 export default function RootLayout({
@@ -22,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className={robotoSlab.className}>{children}</body>
     </html>
   );
