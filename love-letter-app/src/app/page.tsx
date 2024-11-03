9cef3401094e4de9684ec2fade6840c58d99d315
konradmh3 <konradm.h.3@gmail.com>
2024-11-02 18:00:16 -0700

    Fix

--- love-letter-app/src/app/page.tsx
+++ love-letter-app/src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center">
         <Link href="/create">
           <Image
             className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] hover:invert" 
